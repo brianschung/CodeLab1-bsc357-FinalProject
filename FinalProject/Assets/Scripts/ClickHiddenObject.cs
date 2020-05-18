c9b09b15ae07929ff999a47455164dcbcbe2b683
Brian <briansangilchung@gmail.com>
2020-05-18 08:02:58 -0400

    prototype commit

---
+++ FinalProject/Assets/Scripts/ClickHiddenObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickHiddenObject : MonoBehaviour
+{
+
+    public static string NameOfObject; // string that's the name of the object
+    public Transform SuccessfulClick; // the position of the successfully clicked hidden object
+
+    public GameObject ObjectNameText; // the UI text label object that is associated with this hidden object
+    public Transform ObjectNameTextPos; // the position of the associated UI text label object
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // WHEN OBJECT IS CLICKED
+    void OnMouseDown()
+    {
+        //DATA MANAGEMENT
+        NameOfObject = gameObject.name; // set the name of the object
+        Debug.Log(NameOfObject);
+        FindObjectOfType<GameManager>().FoundObject(NameOfObject);
+
+        //FEEDBACK ON FOUND OBJECT
+        Instantiate(SuccessfulClick, gameObject.GetComponent<Transform>().position, SuccessfulClick.rotation); // particle effect where the hidden object is
+        // Instantiate(SuccessfulClick, ObjectNameTextPos.position, SuccessfulClick.rotation); // particle effect where the text label is
+         // sound effect
+        
+
+        //REMOVE OBJECT / STRIKE FROM LIST
+        //Destroy(gameObject); // destroy the object once clicked
+        ObjectNameText.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Strikethrough; // strikethrough the UI text label
+        //Destroy(ObjectNameText); // destroy the associated UI object once clicked
+
+    }
+    
+
+
+
+}
+
---
+++ FinalProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // holds the singleton
+
+    private float timer = 0; // time variable
+    private TextMeshProUGUI timeChangingText; // variable to hold component
+    public GameObject timerText; // the timer ui GameObject
+
+    private int NumberOfClicks; // variable for the number of clicks
+    private TextMeshProUGUI clicksChangingText; // variable to hold the clicks text component
+    public GameObject clickText; // the clicks ui GameObject
+
+    //LIST OF ALL HIDDEN OBJECTS
+    public List<string> foundObjects = new List<string>();
+    private string[] hiddenObjects = { "Bug", "Mug", "PlantZZ", "TV" };
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        { 
+            instance = this; // set instance to be this object
+            DontDestroyOnLoad(gameObject); // don't destroy this object when loading a new scene
+        }
+        else
+        { 
+            Destroy(gameObject); // if this object exists already, destroy (so that there's only 1 total)
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeChangingText = timerText.GetComponent<TextMeshProUGUI>();
+        timer = 0;
+
+        clicksChangingText = clickText.GetComponent<TextMeshProUGUI>();
+        NumberOfClicks = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TIMER
+        timer += Time.deltaTime; // increase the timer by deltaTime every frame
+        timeChangingText.text = "Time: " + (int)timer; // set text component with the time (rounded)
+
+        // CLICKS
+        if (Input.GetMouseButtonDown(0)) // if the player clicks
+        {
+            NumberOfClicks += 1; // increment the number of clicks
+            clicksChangingText.text = "Clicks: " + NumberOfClicks; // function to set the text
+            //Debug.Log("Clicks: " + NumberOfClicks);
+        }
+
+        // PRESS R TO RESTART
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            print("game restarted!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+        // IS THE GAME OVER?
+
+        if (foundObjects.Count == hiddenObjects.Length)
+        {
+            Debug.Log("GAME OVER");
+        }
+
+    }
+
+
+    // WHEN AN OBJECT IS FOUND
+    public bool FoundObject(string key)
+    {
+        if (foundObjects.Contains(key)) return false;
+        foundObjects.Add(key);
+        IsGameOver();
+        return true;
+    }
+
+    // WHETHER THE GAME IS OVER OR NOT
+    public bool IsGameOver()
+    {
+        return foundObjects.Count == hiddenObjects.Length; // it's true when the number of found objects matches the total number of hidden objects in the list
+    }
+
+
+}
